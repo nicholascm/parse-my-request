026d41d41b468b769bac54cc3490f37f92eb097e
nicholascm90 <nicholascm90@gmail.com>
2016-03-02 04:32:51 +0000

    home page

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var app = new express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function (req, res) {
+  res.send("<h1>Who am I? </h1><br><h5>Providing the service of identity</h5><br><p><strong>navigate to /whoami to determine who you are</strong></p>"); 
+}); 
 
 app.get('/whoami', function(req, res) {
     res.json(
