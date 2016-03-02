3e85530e203cf33e6e00f69e43d22255ed17a1ff
nicholascm90 <nicholascm90@gmail.com>
2016-03-02 03:54:55 +0000

    lots of stuff

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 //logic for parsing header here
 
+var express = require('express'); 
+
+var app = new express(); 
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(req, res) {
+    res.json(
+            {"ip-address": req.ip,
+            "language": req.headers["accept-language"], 
+            "os": req.headers["user-agent"]
+            }
+        ); 
+}); 
+
+app.get('/request', function(req, res) {
+  console.log(req); 
+  
+}); 
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
